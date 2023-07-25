dec51bd01e489a670486c69bdac81f451b21794e
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-07-25 05:48:59 +0100

    Fixed pycodestyle errors

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""A Python script that, using REST API, 
-for a given employee ID, returns information 
+"""A Python script that, using REST API,
+for a given employee ID, returns information
 about his/her TODO list progress."""
 import requests
 import sys
@@ -10,7 +10,8 @@ if __name__ == "__main__":
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
     todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    completed_todos = [t.get("title") for t in todos if t.get("completed") is True]
+    completed_todos = [t.get("title") for t in todos if t.get("completed")
+                       is True]
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed_todos), len(todos)))
     [print("\t {}".format(c)) for c in completed_todos]
